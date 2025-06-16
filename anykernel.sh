7d1b7a814fda9e949835cf395638656f810441db
igna <148277185+ignaigna@users.noreply.github.com>
2025-06-16 01:10:11 -0400

    add support for Xiaomi Redmi 9 (Lancelot)

--- anykernel.sh
+++ anykernel.sh
@@ -4,17 +4,17 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
-do.devicecheck=1
+kernel.string=UnnamedKernel
+do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
+device.name1=lancelot
+device.name2=Lancelot
+device.name3=shiva
+device.name4=lava
+device.name5=Lava
 supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
@@ -29,7 +29,7 @@ set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
 } # end attributes
 
 # boot shell variables
-BLOCK=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+BLOCK=/dev/block/by-name/boot;
 IS_SLOT_DEVICE=0;
 RAMDISK_COMPRESSION=auto;
 PATCH_VBMETA_FLAG=auto;
